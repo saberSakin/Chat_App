5aef8ace49ecdbca9fe6614276378255d73a0374
unknown <md.shakin18@gmail.com>
2025-01-14 21:15:28 +0600

    database and backend updates

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    // Implementation for signup functionality
+  };
+  
+  export const login = (req, res) => {
+    res.send("login route");
+  };
+  
+  export const logout = (req, res) => {
+    res.send("logout route");
+  };
